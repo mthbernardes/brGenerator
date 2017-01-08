b980054d9aebe123c928e2d902f506ee5a9272ee
Matheus Bernardes <matheus.bernardes@pythonicos.com.br>
2017-01-08 14:07:56 -0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='brGenerator',
+    version='1.1',
+    url='https://github.com/mthbernardes/brGenerator',
+    license='MIT License',
+    author='Matheus Bernardes',
+    author_email='mthbernardes@gmail.com',
+    keywords='gerador pessoas',
+    description=u'Modulo para gerar informações de pessoas, util para automação de cadastros/',
+    packages=['brGenerator'],
+    install_requires=['requests','lxml'],
+)
